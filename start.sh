2d551dbb181b71f64704fb6c9751225f8dc16b9b
hwp <hwppippo@gmail.com>
2017-09-08 13:31:49 +0800

    commit docker start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 RESUME_PATH="$HOME/work/comm_doc/resume"
-RESUME_PORT="8080"
+RESUME_PORT="9080"
 
 command -v docker >/dev/null 2>&1
 if [ $? != 0 ]; then curl -sSL https://get.docker.com/ | sh; fi
 
 mkdir -p $RESUME_PATH
-docker run -d --name resume_tmp hwppippo/resume
+docker run -d --name resume_tmp zuolan/resume
 if [ ! -d "$RESUME_PATH/pages" ]; then
   docker cp resume_tmp:/usr/html/user/config $RESUME_PATH/config
 fi
@@ -17,5 +17,5 @@ docker rm -f resume_tmp resume >/dev/null 2>&1
 docker run -d --name resume -p $RESUME_PORT:80 \
     -v $RESUME_PATH/pages:/usr/html/user/pages \
     -v $RESUME_PATH/config/:/usr/html/user/config/ \
-    --restart=always hwppippo/resume
+    --restart=always zuolan/resume
 echo "Done"
